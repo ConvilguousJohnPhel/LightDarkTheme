9386de7c2379186e8921d0ee4209ad540a25de96
ConvilguousJohnPhel <johnphel.detorres@convilguous.com>
2020-07-24 16:50:20 +0200

    Initial commit

---
+++ LightDarkTheme/App.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Windows;
+namespace LightDarkTheme
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const string RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+
+        private const string RegistryValueName = "AppsUseLightTheme";
+
+        public enum Theme
+        {
+            Light,
+            Dark
+        }
+
+        private ResourceDictionary ThemeDictionary
+        {
+            get { return Resources.MergedDictionaries[0]; }
+            set { Resources.MergedDictionaries[0] = value; }
+        }
+
+        private void ChangeTheme(Uri uri)
+        {
+            ThemeDictionary = new ResourceDictionary() { Source = uri };
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            SetTheme();
+            MainWindow ParentWindow = new MainWindow();
+            ParentWindow.CurrentApplication = this;
+            this.MainWindow = ParentWindow;
+            ParentWindow.Show();
+        }
+
+        public string GetWindowsTheme()
+        {
+            var currentUser = WindowsIdentity.GetCurrent();
+            string query = string.Format(
+                CultureInfo.InvariantCulture,
+                @"SELECT * FROM RegistryValueChangeEvent WHERE Hive = 'HKEY_USERS' AND KeyPath = '{0}\\{1}' AND ValueName = '{2}'",
+                currentUser.User.Value,
+                RegistryKeyPath.Replace(@"\", @"\\"),
+                RegistryValueName);
+
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(RegistryKeyPath))
+            {
+                object registryValueObject = key?.GetValue(RegistryValueName);
+                if (registryValueObject == null)
+                {
+                    return "LightTheme";
+                }
+
+                int registryValue = (int)registryValueObject;
+
+                return registryValue == 0 ? "LightTheme" : "DarkTheme";
+            }
+        }
+
+        public void SetTheme()
+        {
+            //string ThemeName = "";
+
+            //switch (ThemeOption)
+            //{
+            //    case Theme.Dark: ThemeName = "DarkTheme"; break;
+            //    case Theme.Light: ThemeName = "LightTheme"; break;
+            //}
+
+            try
+            {
+                if (!string.IsNullOrEmpty(GetWindowsTheme()))
+                    ChangeTheme(new Uri($"Themes/{GetWindowsTheme()}.xaml", UriKind.Relative));
+            }
+            catch (Exception eee)
+            {
+            }
+        }
+    }
+}
---
+++ LightDarkTheme/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Iwwerall;
+
+namespace LightDarkTheme
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AlgemeneFuncties AF = new AlgemeneFuncties();
+        public App CurrentApplication { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void ChangeTheme(object sender, RoutedEventArgs e)
+        //{
+        //    switch (int.Parse(((MenuItem)sender).Uid))
+        //    {
+        //        // light
+        //        case 1: CurrentApplication.SetTheme(App.Theme.Light); break;
+        //        // colourful light
+        //        // dark
+        //        case 2: CurrentApplication.SetTheme(App.Theme.Dark); break;
+        //        // colourful dark
+        //    }
+        //    e.Handled = true;
+        //}
+    }
+}
